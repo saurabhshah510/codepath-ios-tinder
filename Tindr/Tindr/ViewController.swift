1a6834f9d2bb972a3d64230c5808c878fbcb4fdf
sshah1 <sshah0@walmartlabs.com>
2015-10-08 16:18:00 -0700

    Initial tinder xcode project commit

---
+++ Tindr/Tindr/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tindr
+//
+//  Created by Saurabh Shah on 10/8/15.
+//  Copyright © 2015 Saurabh Shah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
